fb7a3d934aa28f1ba2c806dddcded94e38fb026c
AmaseCocoa <cocoa@amase.cc>
2025-10-28 23:21:47 +0900

    Revert "2"
    This reverts commit 91048fd116a3bff7e2cf13e7cf3b1249aca3b7f1.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import endpoints
 from apkit.config import AppConfig
 from apkit.server import ActivityPubServer
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+import endpoints
 
 config = AppConfig()
 app = ActivityPubServer()
@@ -14,12 +15,5 @@ app.include_router(endpoints.router)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    context = {
-        "request": request,
-        "fdv_host": f"{request.url.scheme}://{request.url.hostname}{':' + str(request.url.port) if request.url.port != 80 and request.url.port != 443 else ''}",
-    }
-    return templates.TemplateResponse(
-        "index.html",
-        context,
-        headers={"Cache-Control": "max-age=604800, public"},
-    )
+    context = {"request": request, "fdv_host": f"{request.url.scheme}://{request.url.hostname}{':' + str(request.url.port) if request.url.port != 80 and request.url.port != 443 else ''}"}
+    return templates.TemplateResponse("index.html", context, headers={"Cache-Control": "max-age=604800, public"})
